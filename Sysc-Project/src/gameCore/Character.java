51fee47ed4ccb4a807b8a628d099077b017187fc
trangpham <trangpham@cmail.carleton.ca>
2012-10-16 17:03:50 -0400

    Files that Karen added to master

--- Sysc-Project/src/gameCore/Character.java
+++
@@ -1,6 +0,0 @@
-package gameCore;
-
-public class Character {
-	
-
-}
--- Sysc-Project/src/gameCore/Weapon.java
+++
@@ -1,9 +0,0 @@
-package gameCore;
-
-public class Weapon extends Item {
-	private int attackValue;
-	public Weapon(String name, int weight, int attackValue){
-		super(name,weight);
-		this.attackValue=attackValue;
-	}
-}
