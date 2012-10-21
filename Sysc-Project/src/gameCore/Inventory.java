5539b3e0c6c856915d53762081b252701aa2b6fb
kmadore <kmadore@NB_M6400>
2012-10-21 13:30:58 -0400

    Modified inventory, item, character to support pickUP method in Player. Added NPC interface and Monster class.

--- Sysc-Project/src/gameCore/Inventory.java
+++ Sysc-Project/src/gameCore/Inventory.java
@@ -19,34 +19,29 @@ import java.util.List;
  */
 
 public class Inventory {
+	//------------Fields------------//
 	private List<Item> items;
-	/*
-	 * Constr
-	 */
+	
+
+	//------------Constructors------------//
 	public Inventory(){
 		items= new ArrayList<Item>();
 	}
 	
+	//------------getters/setters------------//
 	public void addItem(Item i){
 		items.add(i);
 	}
 	
-	public void removeItem(Item i){
-		items.remove(i);
-	}
-	
-	public boolean containsItem(Item i){
-		if(items.contains(i)){
-			return true;
+	/*
+	 * Given the index in the inventory, retrieve and return the Item from the Inventory.
+	 * @return item at the given index
+	 */
+	public Item getItem(int index){
+		if(index>items.size()-1){
+			return null;
 		}else{
-			return false;
-		}
-	}
-	public boolean isEmpty(){
-		if (items.size()==0){
-			return true;
-		}else{
-			return false;
+			return items.get(index);
 		}
 	}
 	
@@ -56,5 +51,61 @@ public class Inventory {
 			total +=i.getWeight();
 		}
 		return total;
+	}
+	
+	public void removeItem(Item i){
+		items.remove(i);
+	}
+	
+	//------------Status------------//
+	/*
+	 * Get the number of items in the inventory
+	 * @return count of items contained in the inventory
+	 */
+	public int size(){
+		return items.size();
+	}
+	
+	public boolean containsItem(Item i){
+		if(items.contains(i)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/*
+	 * Query for the index of the item in the inventory by name
+	 * @return index of item found in the inventory, -1 if item is not in the inventory
+	 */
+	
+	public int getIndex(String s){
+		int index=0;
+		for (Item i: items){
+			if(i.getName()==s) return index;
+			index++;
+		}
+		return -1;
+	}
+	
+	public boolean isEmpty(){
+		if (items.size()==0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	//------------standard/Misc------------//
+	/*
+	 * Returns the string representation of the inventory
+	 * @return	names of all items in the inventory
+	 */
+	public String toString(){
+		String s="";
+		for(Item i: items){
+			s+=i.toString() +" ";
+		}
+		return s;
 	}
 }
--- Sysc-Project/src/gameCore/Player.java
+++ Sysc-Project/src/gameCore/Player.java
@@ -1,5 +1,7 @@
+
 package gameCore;
 
+import java.util.Scanner;
 /**
  * A Character is a superclass for all of the animated creatures inside the game.
  * Player, and Monsters (subclass of NPC) are subclasses of character.
@@ -32,15 +34,64 @@ public class Player extends Character
 		super(name,health,attack,myPosition);
 		this.stamina=stamina;
 	}
+	
 	/*
-	 *  
+	 * Moves the player in the direction specified by the direction string.  ie north, south, east,etc
+	 * @param direction - direction string ie north, south, etc.
 	 */
 	public void move(String direction){
-		Edge nextEdge;
+		
 		if(myPosition.canMove(direction)){
-			
-			// temporarily commented out to test push .... km nextEdge=myPosition.getEdge(direction);
-			// I was here....nextEdge.cross(myPosition, this); //cross to the next tile
+			this.myPosition=myPosition.moveCharacter(direction);  //move character to the next tile
 		}
+	}
+	
+	/*
+	 * Prompt the player to choose the item to pick up
+	 * @return the item chosen 
+	 */
+	private int promptForItem(){
+		Scanner s;
+		String itemName;
+			
+		String pString="Items available for pick-up are: ";
+		pString += this.inventory.toString()+ "\n";
+		pString+="Please enter the name of the item to pick-up.\n";
+		System.out.println(pString);
+		
+		s = new Scanner(System.in);
+		itemName=s.next();
+		int index = myPosition.getInventory().getIndex(itemName);
+		if (index>=0){
+			return index;
+		}else{
+			System.out.println("No such item to pick up.");
+			return -1;
+		}
+	}
+	
+	/*
+	 * Pickup method picks up the item on the current tile
+	 * -- present a list of items in the inventory for selection if more than one item in the inventory, 
+	 * -- otherwise, pick-up the item and add it to the player's inventory
+	 */
+	public void pickUpItem(){
+		switch (myPosition.getInventory().size()){
+		case 0:
+			System.out.printf("Nothing to pick-up.");
+			break;
+		case 1: //pick-up the single item from the tile
+			this.inventory.addItem(myPosition.getInventory().getItem(0));
+			break;
+		default:
+			int i=promptForItem();
+			if(i>=0){ //add it to my inventory and call tile to remove it from its inventory
+				Item iToPickup = myPosition.getInventory().getItem(i);
+				this.inventory.addItem(iToPickup);  //add to my inventory
+				myPosition.removeItem(iToPickup);	//call tile to remove from its inventory
+				
+			}
+			break;
+		}		
 	}
 }
