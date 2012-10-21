75f6f68b60883b20a2b72129dfe8c70ca387c672
kmadore <kmadore@NB_M6400>
2012-10-21 00:49:53 -0400

    Testing commit and push per TA.

--- Sysc-Project/src/gameCore/Item.java
+++ Sysc-Project/src/gameCore/Item.java
@@ -1,6 +1,21 @@
 package gameCore;
-
+/**
+ * A Item is a basic unit that can be carried in the Inventory. 
+ *  
+ * @author Group D
+ * @author Main author: Karen Madore
+ * 
+ * Group D Members
+ * ---------------
+ * Karen Madore
+ * Trang Pham
+ * Darrell Penner
+ * 
+ *
+ * @version 1.0
+ */
 public class Item {
+	
 	private String name;
 	private int weight;
 	
@@ -9,10 +24,21 @@ public class Item {
 		this.weight=weight;
 	}
 	
+	//------------Getters------------//
+	/*
+	 * Method to get the weight of the item.
+	 * 
+	 * @return	the weight of the item 
+	 */
 	public int getWeight(){
 		return weight;
 	}
 	
+	/*
+	 * Method to the name of the item
+	 * 
+	 * @return	the name of the item
+	 */
 	public String getName(){
 		return name;
 	}
