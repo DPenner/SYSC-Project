1e0d1f16f54038463ebd8f3782f3b56340bf6b7f
kmadore <kmadore@NB_M6400>
2012-10-22 08:07:07 -0400

    Completed use(i, c) in character.  Cleaned up and documented other classes.

--- Sysc-Project/src/gameCore/Weapon.java
+++ Sysc-Project/src/gameCore/Weapon.java
@@ -1,13 +1,41 @@
 package gameCore;
-
+/**
+ * A Weapon is a type of item that has an attack to increase the character's attack value.
+ * 
+ * @author Group D
+ * @author Main author: Karen Madore
+ * 
+ * Group D Members
+ * ---------------
+ * Karen Madore
+ * Trang Pham
+ * Darrell Penner
+ * 
+ *
+ * @version 1.0
+ */
 public class Weapon extends Item {
+	//------------Fields------------//
 	private int attackValue;
 	
+	//------------Constructors------------//
+	/**
+	 * Construct a weapon.
+	 * @param name - name of the weapon
+	 * @param weight - weight of the weapon
+	 * @param attackValue - weapon's attackValue
+	 */
 	public Weapon(String name, int weight, int attackValue){
 		super(name, weight);
 		this.attackValue=attackValue;
 	}
 	
+
+	//------------Getters------------//
+	/**
+	 * Get the attack value of the weapon
+	 * @return attack value of the weapon
+	 */
 	public int getAttackValue(){
 		return attackValue;
 	}
