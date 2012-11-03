ba9c5e8326de33fd12f09d573c781497dc8e8f04
kmadore <kemadore@gmail.com>
2012-11-03 16:37:54 -0400

    Added Character Tests.

--- Sysc-Project/src/jUnitTests/CharacterTest.java
+++ Sysc-Project/src/jUnitTests/CharacterTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 import java.awt.Point;
 
+import gameCore.Inventory;
+import gameCore.Item;
 import gameCore.Room;
 import gameCore.Tile;
 import gameCore.Character;
@@ -31,8 +33,19 @@ import org.junit.Test;
  * @version 1.0
  */
 public class CharacterTest {
+	private static final int ATTACKER_INITIAL_ATTACK = 2;
+	private static final int ATTACKER_INITIAL_HEALTH = 10;
+	private static final int DEFENDER_INITIAL_ATTACK = 1;
+	private static final int DEFENDER_INITIAL_HEALTH = 8;
 	private Room room;
-	private Tile myPosition;
+	private Tile position1;
+	private Tile position2;
+	private Tile position3;
+	private Tile charPosition;
+	private Inventory cInventory;
+	private Character testCharacter;
+	private Character deadCharacter;
+	
 	/**
 	 * Before the character can be created, a Tile and the room the tile
 	 * resides must be instantiated before the character can be created.
@@ -43,8 +56,16 @@ public class CharacterTest {
 	@Before
 	public void setUp() throws Exception {
 		room = new Room();
-		Point position = new Point(0,0);
-		myPosition=new Tile(position, room);
+		Point p1 = new Point(0,0);
+		Point p2 = new Point(0,1);
+		Point p3 = new Point(0,2);
+		Point p4 = new Point(1,0);
+		position1=new Tile(p1, room);
+		position2 = new Tile(p2, room);
+		charPosition = new Tile(p3, room);
+		position3= new Tile(p4, room);
+		cInventory= new Inventory();
+		testCharacter=new Character("TestCharacter", DEFENDER_INITIAL_HEALTH, DEFENDER_INITIAL_ATTACK, charPosition);
 	}
 
 	/**
@@ -52,6 +73,7 @@ public class CharacterTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	/**
@@ -59,9 +81,7 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testCharacter() {
-		Character c=new Character("Test Character", 10, 2, myPosition);
-		
-		fail("Not yet implemented");
+		assertNotNull("Newly instantiated test character should not be null.", testCharacter);
 	}
 
 	/**
@@ -69,12 +89,32 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testAttack() {
-		fail("Not yet implemented");
+		Character defender=new Character("Defender", DEFENDER_INITIAL_HEALTH, DEFENDER_INITIAL_ATTACK, position2);
+		Character attacker=new Character("Attacker", ATTACKER_INITIAL_HEALTH, ATTACKER_INITIAL_ATTACK, position3);
+		//first attack
+		attacker.attack(defender);//ie player attacks monster who is the defender
+		assertEquals("Attacker health should be 9.", 9, attacker.getHealth());
+		assertEquals("Defender health should be 6.", 6, defender.getHealth());
+		//second attack
+		attacker.attack(defender);//ie player attacks monster who is the defender
+		assertEquals("Attacker health should be 8.", 8, attacker.getHealth());
+		assertEquals("Defender health should be 4.", 4, defender.getHealth());
+		//third attack
+		attacker.attack(defender);//ie player attacks monster who is the defender
+		assertEquals("Attacker health should be 7.", 7, attacker.getHealth());
+		assertEquals("Defender health should be 2.", 2, defender.getHealth());
+		//fourth attack
+		attacker.attack(defender);//ie player attacks monster who is the defender
+		assertEquals("Attacker health should be 6.", 6, attacker.getHealth());
+		assertEquals("Defender health should be 0.", 0, defender.getHealth());
+		assertTrue("Defender should be dead now. ", defender.isDead());
 	}
 
-	/**
+	/*
 	 * Test method for {@link gameCore.Character#use(gameCore.Item)}.
-	 */
+	 * 
+	 * --- not used in version 2 ---
+	 *
 	@Test
 	public void testUseItem() {
 		fail("Not yet implemented");
@@ -82,7 +122,9 @@ public class CharacterTest {
 
 	/**
 	 * Test method for {@link gameCore.Character#use(gameCore.Item, gameCore.Character)}.
-	 */
+	 * 
+	 * --- not used in version 2 ---
+	 *
 	@Test
 	public void testUseItemCharacter() {
 		fail("Not yet implemented");
@@ -93,15 +135,21 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testAdjustHealth() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link gameCore.Character#die()}.
-	 */
-	@Test
-	public void testDie() {
-		fail("Not yet implemented");
+			
+		//test negative value decrease character's health 
+		int reduceHealthbyTwo = -2;
+		testCharacter.adjustHealth(reduceHealthbyTwo);
+		assertEquals("Test Character's health should be 6.", 6, testCharacter.getHealth());
+		
+		//ensure health does not fall below 0
+		int reduceHealthbyTwenty = -20;
+		testCharacter.adjustHealth(reduceHealthbyTwenty);
+		assertEquals("Test Character's health should be 0.", 0, testCharacter.getHealth());
+		
+		//test increasing player health
+		testCharacter.adjustHealth(5);
+		assertEquals("Test Character's health should be 5.", 5, testCharacter.getHealth());
+		
 	}
 
 	/**
@@ -109,7 +157,17 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testDropItem() {
-		fail("Not yet implemented");
+		//no item to drop so Tile should not have anything
+		Item RedKey = new Item("RedKey", 1);
+		
+		assertFalse("The tile should NOT have an item.", charPosition.hasItems());
+		
+		testCharacter.dropItem(RedKey);
+		
+		assertFalse("RedKey item dropped.", testCharacter.hasItem(RedKey));
+		assertTrue("The tile should now have an item.", charPosition.hasItems());
+		
+		
 	}
 
 	/**
@@ -117,7 +175,12 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testHasItem() {
-		fail("Not yet implemented");
+		Item RedKey = new Item("RedKey", 1);
+		testCharacter.addItem(RedKey);
+		assertTrue("Character should have a RedKey item.", testCharacter.hasItem(RedKey));
+		
+		testCharacter.dropItem(RedKey);
+		assertFalse("Character should NOT have a RedKey item.", testCharacter.hasItem(RedKey));
 	}
 
 	/**
@@ -125,7 +188,17 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testIsDead() {
-		fail("Not yet implemented");
+		Character deadCharacter=new Character("DeadCharacter", 0, 0, position1);
+		assertEquals("Character is with health 0 is dead.", 0, deadCharacter.getHealth());
+		assertTrue("Character is dead.", deadCharacter.isDead());
+	}
+	/**
+	 * Test isDead method returns false if character is alive.
+	 */
+	@Test
+	public void testIsAlive(){
+		Character aliveCharacter=new Character("Alive Character", 1, 0, position2);
+		assertFalse("Character is not dead.", aliveCharacter.isDead());
 	}
 
 	/**
@@ -133,7 +206,9 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testGetInventoryString() {
-		fail("Not yet implemented");
+		Item RedKey=new Item("RedKey",1);
+		testCharacter.addItem(RedKey);
+		assertNotNull("A string should be returned.  empty string if inventory does not contain any items.", testCharacter.getInventory());
 	}
 
 	/**
@@ -141,7 +216,9 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testGetInventory() {
-		fail("Not yet implemented");
+		Item RedKey=new Item("RedKey",1);
+		testCharacter.addItem(RedKey);
+		assertNotNull("Character should have an inventory and getInventory should return it.", testCharacter.getInventory());
 	}
 
 	/**
@@ -149,7 +226,10 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testAddItem() {
-		fail("Not yet implemented");
+		
+		Item RedKey=new Item("RedKey",1);
+		testCharacter.addItem(RedKey);
+		assertNotNull("Character should have an inventory and getInventory should return it.", testCharacter.getInventory());
 	}
 
 	/**
@@ -157,7 +237,7 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		assertNotNull("It should return string representing Test Character.", testCharacter.toString());
 	}
 
 	/**
@@ -165,7 +245,7 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testGetAttack() {
-		fail("Not yet implemented");
+		
 	}
 
 	/**
@@ -173,7 +253,8 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testSetAttack() {
-		fail("Not yet implemented");
+		testCharacter.setAttack(20);
+		assertEquals("Set test character's health to 20.", 20, testCharacter.getAttack());
 	}
 
 	/**
@@ -181,7 +262,7 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testGetHealth() {
-		fail("Not yet implemented");
+		assertEquals("Test characters health.", DEFENDER_INITIAL_HEALTH, testCharacter.getHealth());
 	}
 
 	/**
@@ -189,7 +270,8 @@ public class CharacterTest {
 	 */
 	@Test
 	public void testSetHealth() {
-		fail("Not yet implemented");
+		testCharacter.setHealth(11);
+		assertEquals("Test setting character's health to 11.", 11, testCharacter.getHealth());
 	}
 
 }
