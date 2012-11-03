b4eb3355d4479e79fe58847413454df483ad585f
kmadore <kemadore@gmail.com>
2012-11-03 13:57:08 -0400

    Added InventoryTests.

--- Sysc-Project/src/jUnitTests/InventoryTest.java
+++ Sysc-Project/src/jUnitTests/InventoryTest.java
@@ -19,7 +19,9 @@ import org.junit.Test;
  */
 public class InventoryTest {
 	//------------Fields------------//
-	private Inventory inv;
+	private Inventory inventoryList;
+	private Item RedKey;
+	private Item BlueKey;
 	
 	/**
 	 * Setup: 
@@ -29,7 +31,9 @@ public class InventoryTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		inv = new Inventory();
+		inventoryList = new Inventory();
+		RedKey = new Item("RedKey",1); //RedKey w:1
+		BlueKey = new Item("BlueKey",1); //BlueKey w:1
 	}
 
 
@@ -38,6 +42,10 @@ public class InventoryTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		for(int i=0; i<inventoryList.size();i++){
+			Item itemToRemove=inventoryList.getItem(i);
+			inventoryList.removeItem(itemToRemove);
+		}
 	}
 
 	/**
@@ -45,18 +53,18 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testInventory() {
-		assertEquals("Size of empty Inventory is 0.", 0, inv.size());
+		assertEquals("Size of empty Inventory is 0.", 0, inventoryList.size());
 	}
 
 	/**
 	 * Test method for {@link gameCore.Inventory#addItem(gameCore.Item)}.
 	 */
 	@Test
-	public void testAddItem() {
+	public void testAddOneItem() {
 		Item item1=new Item("RedKey", 1);
-		inv.addItem(item1);
+		inventoryList.addItem(item1);
 		
-		assertEquals("Size of one item in the inventory should be 1.", 1, inv.size());
+		assertEquals("Size of one item in the inventory should be 1.", 1, inventoryList.size());
 	}
 
 	/**
@@ -64,7 +72,8 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testGetItem() {
-		fail("Not yet implemented");
+		inventoryList.addItem(RedKey);
+		assertEquals("Item retrieved should be RedKey.", RedKey, inventoryList.getItem(0));
 	}
 
 	/**
@@ -72,7 +81,10 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testGetTotalWeight() {
-		fail("Not yet implemented");
+		inventoryList.addItem(BlueKey);
+		assertEquals("Total weight of inventory with BlueKey is 1.", 1, inventoryList.getTotalWeight());
+		inventoryList.addItem(RedKey);
+		assertEquals("Total weight of inventory with BlueKey and RedKey is 2.",2, inventoryList.getTotalWeight());
 	}
 
 	/**
@@ -80,7 +92,9 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testRemoveItem() {
-		fail("Not yet implemented");
+		inventoryList.addItem(BlueKey);
+		inventoryList.removeItem(BlueKey);
+		assertFalse("Inventory list should not contain removed BlueKey.", inventoryList.containsItem(BlueKey));
 	}
 
 	/**
@@ -88,7 +102,11 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		assertEquals("Empty inventory should return size 0.", 0,inventoryList.size());
+		inventoryList.addItem(RedKey);
+		assertEquals("Size of inventory with one item added should return 1.", 1, inventoryList.size());
+		inventoryList.addItem(BlueKey);
+		assertEquals("Size of inventory with two items added should return 2.", 2, inventoryList.size());
 	}
 
 	/**
@@ -96,7 +114,8 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testContainsItem() {
-		fail("Not yet implemented");
+		inventoryList.addItem(RedKey);
+		assertTrue("Inventory should contain item added RedKey.", inventoryList.containsItem(RedKey));
 	}
 
 	/**
@@ -104,7 +123,10 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testGetIndex() {
-		fail("Not yet implemented");
+		inventoryList.addItem(BlueKey);
+		inventoryList.addItem(RedKey);
+		assertEquals("Index of BlueKey is 0.", 0, inventoryList.getIndex("BlueKey"));
+		assertEquals("Index of RedKey is 1.", 1, inventoryList.getIndex("RedKey"));
 	}
 
 	/**
@@ -112,7 +134,9 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
+		assertTrue("Inventory  is empty.", inventoryList.isEmpty());
+		inventoryList.addItem(BlueKey);
+		assertFalse("Inventory with one item in it is not empty.", inventoryList.isEmpty());
 	}
 
 	/**
@@ -120,8 +144,17 @@ public class InventoryTest {
 	 */
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals("A empty string should be returned for empty inventory.", "", inventoryList.toString());
+		inventoryList.addItem(BlueKey);
+		assertNotNull("A string should be returned.", inventoryList.toString());
 	}
 
+	/**
+	 * Use this if there are multiple tests to run and they are combined into a test suite.
+	 * @param args
+	 */
+	public static void main(String[] args){
+		//junit.textui.TestRunner.run(AllInventoryTests.suite());
+	}
 	
 }
