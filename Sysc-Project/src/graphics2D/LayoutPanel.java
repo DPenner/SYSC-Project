c918048a30cffc6e2cfee5abc775bd2c954035a9
DPenner <darrellpenner@hotmail.com>
2012-11-04 19:24:38 -0500

    Minor - error checking

--- Sysc-Project/src/graphics2D/LayoutPanel.java
+++ Sysc-Project/src/graphics2D/LayoutPanel.java
@@ -29,6 +29,9 @@ abstract class LayoutPanel<T extends LayoutObject> extends JPanel implements Obs
 		buffer = new ConcurrentLinkedQueue<T>();
 	}
 	
+	/**
+	 * Paints every layoutObject that this layoutPanel is observing
+	 */
 	@Override
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
@@ -37,14 +40,32 @@ abstract class LayoutPanel<T extends LayoutObject> extends JPanel implements Obs
 		}
 	}
 	
+	/**
+	 * Draws the 
+	 * @param g
+	 * @param layoutObject
+	 */
 	protected abstract void drawLayoutObject(Graphics g, T layoutObject);
 
+	/**
+	 * Repaints the object that this LayoutPanel is observing
+	 */
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		T layoutObject = (T) arg0;
+		T layoutObject;
+		try {
+			layoutObject = (T) arg0; //can't use instanceof with generic type, need try-catch block
+		}
+		catch (ClassCastException e){
+			throw new IllegalArgumentException("The LayoutPanel was observing something that was not a LayoutObject");
+		}
 		repaint(getRepaintRectangle(layoutObject));
 	}
 	
+	/**
+	 * Adds a layoutObject for this layoutPanel to observe
+	 * @param layoutObject The object to observe
+	 */
 	protected void addLayoutObject(T layoutObject){
 		if (layoutObject == null){
 			throw new IllegalArgumentException("layoutObject cannot be null");
@@ -61,5 +82,10 @@ abstract class LayoutPanel<T extends LayoutObject> extends JPanel implements Obs
         });
 	}
 	
+	/**
+	 * Gets the area which needs to be repainted for a given object.
+	 * @param layoutObject The object which needs to be repainted
+	 * @return A Rectangle encompassing the area to be repainted
+	 */
 	protected abstract Rectangle getRepaintRectangle(T layoutObject);
 }
