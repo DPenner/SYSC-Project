f2fdff6b951f0eb8c1f00d3abfb22b5f75550d5e
DPenner <darrellpenner@hotmail.com>
2012-11-20 18:15:35 -0500

    New Monster class test stub.

---
+++ Sysc-Project/src/jUnitTests/MonsterTest.java
@@ -0,0 +1,6 @@
+package jUnitTests;
+
+public class MonsterTest extends CharacterTest {
+	// Monster currently has no active code (methods are there, but not yet complete and not yet being used)
+	//It is therefore just another test of Character
+}
